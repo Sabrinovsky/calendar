b2ac1fab8fc88afef93c54532f8a0dc41bb3ae6e
Matheus Sabino Souza <matheus.souza@ctw.bmwgroup.com>
2023-01-10 15:21:19 +0000

    Again

--- src/Calendar/utils/index.spec.ts
+++ src/Calendar/utils/index.spec.ts
@@ -7,13 +7,13 @@ describe('generateCalendar', () => {
 
   it('returns an array with the current month', () => {
     const response = generateCalendar();
-    console.log({ response });
+
     expect(response).toHaveLength(35);
-    expect(response[0]).toStrictEqual(
-      new Date('2020-05-30T23:00:00.000Z')
+    expect(response[0].toISOString()).toBe(
+      new Date('2020-05-30T23:00:00.000Z').toISOString()
     );
-    expect(response[34]).toStrictEqual(
-      new Date('2020-07-03T23:00:00.000Z')
+    expect(response[34].toISOString()).toBe(
+      new Date('2020-07-03T23:00:00.000Z').toISOString()
     );
   });
 });
