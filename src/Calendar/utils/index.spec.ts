6dd16e9f5d82f0c199e5d63280fbc4aaeea1db4e
Matheus Sabino Souza <matheus.souza@ctw.bmwgroup.com>
2023-01-10 15:45:58 +0000

    Fix again with hours

--- src/Calendar/utils/index.spec.ts
+++ src/Calendar/utils/index.spec.ts
@@ -11,10 +11,10 @@ describe('generateCalendar', () => {
     console.log(new Date())
     expect(response).toHaveLength(35);
     expect(response[0].toISOString()).toBe(
-      new Date('2020-05-30T23:00:00.000Z').toISOString()
+      new Date('2020-05-31T04:00:00.000Z').toISOString()
     );
     expect(response[34].toISOString()).toBe(
-      new Date('2020-07-03T23:00:00.000Z').toISOString()
+      new Date('2020-07-04T04:00:00.000Z').toISOString()
     );
   });
 });
