312bd9f1c6bcd2ef750d347a5ded06b71dc00142
Matheus Sabino Souza <matheus.souza@ctw.bmwgroup.com>
2025-01-24 08:25:59 +0000

    .

--- src/Calendar/components/WeekCalendar/WeekCalendar.tsx
+++ src/Calendar/components/WeekCalendar/WeekCalendar.tsx
@@ -1,22 +1,29 @@
-import { Flex, Text, useDisclosure } from '@chakra-ui/react';
+import { Box, Flex, Text, useDisclosure } from '@chakra-ui/react';
 import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { useCallback, useState } from 'react';
 import { Popover } from '../../../shared/Popover/Popover';
 import { NewEvent } from '../NewEvent/NewEvent';
 
-export default function WeekCalendar({ weekCalendar }: { weekCalendar: Date[] }) {
+export default function WeekCalendar({
+  weekCalendar,
+}: {
+  weekCalendar: Date[];
+}) {
   // const { } = useDisclosure()
-  const [popoverProps, setPopoverProps] = useState<{ day?: Date, hour?: number }>()
+  const [popoverProps, setPopoverProps] = useState<{
+    day?: Date;
+    hour?: number;
+  }>();
   const toggle = useCallback((day: Date, hour: number) => {
-    setPopoverProps({ day, hour })
-  }, [])
+    setPopoverProps({ day, hour });
+  }, []);
   return (
-    <Flex className='animate' flexDirection="column" flex={1}>
+    <Flex className="animate" flexDirection="column" flex={1}>
       <Flex flexDirection="row" flex={1} paddingInline={10}>
         {weekCalendar.map((day, index) => (
           <Flex
-            role='list'
+            role="list"
             className="week"
             flexDirection="column"
             flex={1}
@@ -35,36 +42,55 @@ export default function WeekCalendar({ weekCalendar }: { weekCalendar: Date[] })
             <Flex flex={1} flexDirection="column">
               {Array.from(Array(24).keys()).map((hour) => (
                 <Popover
-                  isOpen={popoverProps?.day === day && popoverProps?.hour === hour}
+                  isOpen={
+                    popoverProps?.day === day && popoverProps?.hour === hour
+                  }
                   key={day.toISOString() + hour}
                   content={
-                    <NewEvent key={day.toISOString() + hour} hour={hour} day={day} />
+                    <NewEvent
+                      key={day.toISOString() + hour}
+                      hour={hour}
+                      day={day}
+                    />
                   }
                 >
-                  <Flex
-                    onClick={() => toggle(day, hour)}
-                    role='listitem'
-                    position="relative"
-                    flex={1}
-                    className="hour"
-                    minHeight={'50px'}
-                  >
-                    {index === 0 && (
-                      <Flex
-                        className="hour-indicator"
-                        top="-0.7rem"
-                        fontSize="0.9rem"
-                        left={-8}
-                        minWidth="2ch"
-                        justifyContent="end"
-                        position="absolute"
-                        color="gray.500"
-                      >
-                        {hour}h
-                      </Flex>
-                    )}
-
-                  </Flex>
+                  {(isOpen) => (
+                    <Flex
+                      onClick={() => toggle(day, hour)}
+                      role="listitem"
+                      position="relative"
+                      flex={1}
+                      paddingTop={0.5}
+                      paddingRight={3}
+                      paddingBottom={0.5}
+                      paddingLeft={0.5}
+                      className="hour"
+                      minHeight={'50px'}
+                    >
+                      <Box
+                        backgroundColor={isOpen ? 'blue.100' : 'white'}
+                        // borderColor={isOpen ? 'blue.300' : 'white'}
+                        // borderWidth={isOpen ? 2 : 0}
+                        flex={1}
+                        borderRadius={4}
+                        boxShadow={isOpen ? 'xl' : 'none'}
+                      ></Box>
+                      {index === 0 && (
+                        <Flex
+                          className="hour-indicator"
+                          top="-0.7rem"
+                          fontSize="0.9rem"
+                          left={-8}
+                          minWidth="2ch"
+                          justifyContent="end"
+                          position="absolute"
+                          color="gray.500"
+                        >
+                          {hour}h
+                        </Flex>
+                      )}
+                    </Flex>
+                  )}
                 </Popover>
               ))}
             </Flex>
--- src/shared/Popover/Popover.tsx
+++ src/shared/Popover/Popover.tsx
@@ -1,21 +1,42 @@
-import { Popover as ChakraPopover, Button, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverTrigger, Portal, useDisclosure, usePopover } from "@chakra-ui/react";
+import {
+  Popover as ChakraPopover,
+  Button,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverTrigger,
+  Portal,
+  useDisclosure,
+  usePopover,
+} from '@chakra-ui/react';
 
-export function Popover({ children, content, isOpen }: { children: React.ReactNode, content: React.ReactNode, isOpen: boolean }) {
-
+export function Popover({
+  children,
+  content,
+  isOpen,
+}: {
+  children: React.ReactNode | ((isOpen: boolean) => React.ReactNode);
+  content: React.ReactNode;
+  isOpen: boolean;
+}) {
   return (
-    <ChakraPopover isLazy={true} lazyBehavior='unmount' isOpen={isOpen} placement="top-end">
+    <ChakraPopover
+      isLazy={true}
+      lazyBehavior="unmount"
+      isOpen={isOpen}
+      placement="top-end"
+    >
       <PopoverTrigger>
-        {children}
+        {typeof children === 'function' ? children(isOpen) : children}
       </PopoverTrigger>
       <Portal>
         <PopoverContent>
           <PopoverArrow />
           <PopoverCloseButton />
-          <PopoverBody>
-            {content}
-          </PopoverBody>
+          <PopoverBody>{content}</PopoverBody>
         </PopoverContent>
       </Portal>
     </ChakraPopover>
-  )
+  );
 }
